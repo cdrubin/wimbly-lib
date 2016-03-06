340b9aa4b3d9b621c26d38bd562767c2d66cbf37
Daniel Rubin <cdrubin@benchmarkeducation.com>
2016-03-06 03:57:20 +0000

    Saturday night

--- sqlbuilder.lua
+++ sqlbuilder.lua
@@ -5,11 +5,12 @@ local SQL = class( 'SQL' )
 
 function SQL:__tostring()
   -- call statement or vice-versa
+  return SQL:statement()
 end
 
 SQL.verb = ''
 SQL.columns = {}
-
+SQL.tables = {}
 
 function SQL:initialize( verb )
   self.verb = verb
@@ -23,8 +24,10 @@ end
 
 function SQL:select( columns )
 
-  for name, alias in pairs( columns ) do
-	if alias == nil then alias = name end
+  for alias, name in pairs( columns ) do
+    --print( '-n-' .. type( name ) )
+	--print( '+a+' .. type( alias ) )
+	if type( alias ) == 'number' then alias = name end
 	self.columns[alias] = name
   end
 
@@ -32,11 +35,37 @@ function SQL:select( columns )
 end
 
 
-function SQL:statement()
-  local statement = "SELECT\n"
+function SQL:from( tables )
 
-  for alias, name in pairs( columns ) do
-    statement = statement .. "\n  " .. name .. " AS " .. alias
+  for alias, name in pairs( tables ) do
+    if type( alias ) == 'number' then alias = name end
+	self.tables[alias] = name
+  end
+
+  return self
+
+end
+
+
+function SQL:statement()
+  local statement = "SELECT"
+
+  for alias, name in pairs( self.columns ) do
+    if alias ~= name then
+      statement = statement .. "\n  " .. name .. ' AS ' .. alias .. ','
+	else
+	  statement = statement .. "\n  " .. name .. ','
+	end
+  end
+
+  statement = statement .. "\nFROM"
+
+  for alias, name in pairs( self.tables ) do
+    if alias ~= name then
+      statement = statement .. "\n  " .. name .. ' ' .. alias .. ','
+	else
+	  statement = statement .. "\n  " .. name .. ','
+	end
   end
 
   return statement
@@ -44,7 +73,17 @@ end
 
 
 --return SQL
-print( SQL.select( { 'id', 'firstname', 'lastname' } ):statement() )
+query = SQL
+  :select( {
+    ['u_id'] = 'id',
+    ['u_firstname'] = 'firstname',
+    ['u_lastname'] = 'lastname'
+  } )
+  :from( {
+    ['user'] = 'u', 'person'
+  } )
 
+print( query )
 
 -- return SQL
+
