5aa3fc0000de33522f0776aa6d04bd31b404d506
Daniel Rubin <cdrubin@benchmarkeducation.com>
2016-03-20 18:17:00 +0000

    All verbs accounted for, JOINS, HAVING and GROUP BY remain

--- sqlbuilder.lua
+++ sqlbuilder.lua
@@ -1,33 +1,70 @@
 
 class = require 'middleclass'
 
+inspect = require 'inspect'
+
+
+
 local SQL = class( 'SQL' )
 
 
-SQL.verb = ''
-SQL.columns = {}
-SQL.tables = {}
-SQL.conditions = {}
 SQL.options = { name_quote = '`' }
 
 
-function SQL:initialize( verb, options )
+function SQL:initialize( verb )
   self.verb = verb
 end
 
 
-function SQL.static:SELECT( columns, options )
-  return SQL:new( 'SELECT', options ):SELECT( columns )
+function SQL.static:SELECT( columns )
+  local sql = SQL:new( 'SELECT' )
+  sql.tables = {}
+  sql.columns = {}
+  self.conditions = {}
+  sql.SET = false
+  return sql:SELECT( columns )
 end
 
 
-function SQL.static:UPDATE( columns, options )
-  return SQL:new( 'UPDATE', options ):SELECT( columns )
+function SQL.static:SELECT_DISTINCT( columns )
+  local sql = SQL:new( 'SELECT DISTINCT' )
+  sql.tables = {}
+  sql.columns = {}
+  self.conditions = {}
+  sql.SET = false
+  return sql:SELECT( columns )
 end
 
 
-function SQL.static:DELETE( columns, options )
-  return SQL:new( 'DELETE', options ):SELECT( columns )
+function SQL.static:UPDATE( tablename )
+  local sql = SQL:new( 'UPDATE' )
+  sql.table = ''
+  sql.values = {}
+  self.conditions = {}
+  sql.FROM = false
+  return sql:UPDATE( tablename )
+end
+
+
+function SQL.static:INSERT( tablename, rows )
+  local sql = SQL:new( 'INSERT INTO' )
+  sql.table = ''
+  sql.rows = {}
+  sql.FROM = false
+  sql.SET = false
+  sql.WHERE = false
+  sql.OR_WHERE = false
+  return sql:INSERT( tablename, rows )
+end
+
+
+function SQL.static:DELETE( tablename )
+  local sql = SQL:new( 'DELETE FROM' )
+  sql.table = ''
+  self.conditions = {}
+  sql.FROM = false
+  sql.SET = false
+  return sql:DELETE( tablename )
 end
 
 
@@ -49,9 +86,14 @@ end
 
 
 function SQL.static:single_quote_and_escape_value( value )
+
   if type( value ) == 'string' then
-    value = "'" .. value:gsub( "'", "''" ) .. "'"
+    -- don't quote NULL, TRUE or FALSE
+    if value ~= 'NULL' and value ~= 'TRUE' and value ~= 'FALSE' then
+      value = "'" .. value:gsub( "'", "''" ) .. "'"
+    end
   end
+
   return value
 end
 
@@ -86,12 +128,77 @@ function SQL:FROM( tables )
 	else
 	  alias = SQL:validate_and_quote_name( alias )
 	end
+
 	self.tables[alias] = name
   end
 
   return self
-
 end
+
+
+function SQL:UPDATE( tablename )
+
+  self.table = SQL:validate_and_quote_name( tablename )
+
+  return self
+end
+
+
+function SQL:SET( values )
+
+  for name, value in pairs( values ) do
+
+    name = SQL:validate_and_quote_name( name )
+    value = SQL:single_quote_and_escape_value( value )
+
+    self.values[name] = value
+  end
+
+  return self
+end
+
+
+function SQL:INSERT( tablename, rows )
+
+  self.table = SQL:validate_and_quote_name( tablename )
+
+  if type( rows ) == 'table' and type ( next( rows ) ) == 'string' then
+    rows = { rows }
+  end
+
+  local names = {}
+  for name, value in pairs( rows[1] ) do
+    table.insert( names, name )
+  end
+
+  self.table = self.table .. "\n  ( "
+
+  for _, name in ipairs( names ) do
+    self.table = self.table .. SQL:validate_and_quote_name( name ) .. ', '
+  end
+  self.table = self.table:sub( 1, -3 ) .. ' )'
+
+  for _, row in ipairs( rows ) do
+
+    local processed_row = {}
+    for _, name in ipairs( names ) do
+      table.insert( processed_row, SQL:single_quote_and_escape_value( row[ name ] ) )
+    end
+
+    table.insert( self.rows, processed_row )
+  end
+
+  return self
+end
+
+
+function SQL:DELETE( tablename )
+
+  self.table = SQL:validate_and_quote_name( tablename )
+
+  return self
+end
+
 
 
 function SQL:OR_WHERE( conditions )
@@ -200,63 +307,98 @@ end
 function SQL:__tostring() return SQL.statement( self ) end
 function SQL:statement()
 
-  local statement = "SELECT"
-
-  for alias, name in pairs( self.columns ) do
-    if alias ~= name then
-      statement = statement .. "\n  " .. name .. ' AS ' .. alias .. ','
-	else
-	  statement = statement .. "\n  " .. name .. ','
-	end
-  end
-
-  statement = statement:sub( 1, -2 ) .. "\nFROM"
+  local statement = self.verb .. ' '
 
 
-  for alias, name in pairs( self.tables ) do
-    if alias ~= name then
-      statement = statement .. "\n  " .. name .. ' ' .. alias .. ','
-	else
-	  statement = statement .. "\n  " .. name .. ','
-	end
-  end
-
-  statement = statement:sub( 1, -2 ) .. "\nWHERE"
-
-  local _recurse_conditions
-  _recurse_conditions = function( conditions, indent )
-
-    local where_statement = ''
-	local conjunction = conditions['conjunction']
-
-	for _, condition in ipairs( conditions ) do
-
-	  local name, relation, value = unpack( condition )
-
-	  if type( name ) == 'table' then
-		where_statement = where_statement .. ' (' .. _recurse_conditions( condition, indent + 2 )
-		  .. "\n" .. (' '):rep( indent + 2) .. ') ' .. conjunction
+  if self.columns then
+	for alias, name in pairs( self.columns ) do
+	  if alias ~= name then
+		statement = statement .. "\n  " .. name .. ' AS ' .. alias .. ','
 	  else
-	    where_statement = where_statement .. "\n  " .. (' '):rep( indent ) .. name .. ' ' .. relation .. ' ' .. value .. ' ' .. conjunction
+		statement = statement .. "\n  " .. name .. ','
 	  end
 	end
-
-    return where_statement:sub( 1, -conjunction:len() - 1 )
   end
 
-  statement = statement .. _recurse_conditions( self.conditions, 0 )
+
+  if self.table then
+	statement = statement .. "\n  " .. self.table .. ' '
+  elseif self.tables then
+	statement = statement:sub( 1, -2 ) .. "\nFROM"
+
+	for alias, name in pairs( self.tables ) do
+	  if alias ~= name then
+		statement = statement .. "\n  " .. name .. ' ' .. alias .. ','
+	  else
+		statement = statement .. "\n  " .. name .. ','
+	  end
+	end
+  end
+
+
+  if self.rows then
+	statement = statement:sub( 1, -2 ) .. "\nVALUES"
+
+	for _, row in ipairs( self.rows ) do
+      statement = statement .. "\n  ( "
+      for _, column in ipairs( row ) do
+        statement = statement .. column .. ', '
+      end
+      statement = statement:sub( 1, -3 ) .. ' ), '
+    end
+
+    statement = statement:sub( 1, -3 )
+  end
+
+
+  if self.values then
+    statement = statement:sub( 1, -2 ) .. "\nSET"
+
+    for name, value in pairs( self.values ) do
+  	statement = statement .. "\n  " .. name .. ' = ' .. value .. ','
+    end
+  end
+
+
+  if #self.conditions > 0 then
+    statement = statement:sub( 1, -2 ) .. "\nWHERE"
+
+    local _recurse_conditions
+    _recurse_conditions = function( conditions, indent )
+
+      local where_statement = ''
+    local conjunction = conditions['conjunction']
+
+    for _, condition in ipairs( conditions ) do
+
+      local name, relation, value = unpack( condition )
+
+      if type( name ) == 'table' then
+      where_statement = where_statement .. ' (' .. _recurse_conditions( condition, indent + 2 )
+        .. "\n" .. (' '):rep( indent + 2) .. ') ' .. conjunction
+      else
+        where_statement = where_statement .. "\n  " .. (' '):rep( indent ) .. name .. ' ' .. relation .. ' ' .. value .. ' ' .. conjunction
+      end
+    end
+
+      return where_statement:sub( 1, -conjunction:len() - 1 )
+    end
+
+    statement = statement .. _recurse_conditions( self.conditions, 0 )
+  end
 
   return statement
 end
 
 
-return SQL
 
---[=[
+---[=[
 
-Example usage:
+--Example usage:
 
-query = SQL
+--SQL.options.name_quote = '|'
+
+local query = SQL
   :SELECT {
     ['id'] = 'u_id',
     ['firstname'] = 'u_firstname',
@@ -314,5 +456,59 @@ query:OR_WHERE{
 
 print( query )
 
+print()
+
+query = SQL
+  :UPDATE( 'user' )
+  :SET{
+    ['id'] = 7,
+    ['firstname'] = firstname,
+    ['lastname'] = lastname,
+	['column'] = 'NULL'
+  }
+  :WHERE{
+    AND = true,   -- <=== default
+	{ 'u.id', '=', 12 },
+	{ 'train', '=', 'due' },
+	{ 'sheep', 'IN', { 9, 78, 78, 90 } }
+  }
+
+print( query )
+
+print()
+
+query = SQL
+  :INSERT( 'user', {
+    { ['email'] = 'TRUE', ['firstname'] = 'moo', ['surname'] = 'loo' },
+    { ['email'] = 'moo2@here.com', ['firstname'] = 'moo2', ['surname'] = 'loo2' }
+  } )
+
+print( query )
+
+
+query = SQL
+  :INSERT( 'user', {
+    ['email'] = 'moo@here.com',
+    ['firstname'] = 'NULL',
+    ['surname'] = 'TRUE'
+  } )
+
+print( query )
+print()
+
+query = SQL
+  :DELETE( 'user' )
+  :WHERE{
+    AND = true,   -- <=== default
+	{ 'u.id', '=', 12 },
+	{ 'train', '=', 'due' },
+	{ 'sheep', 'IN', { 9, 78, 78, 90 } }
+  }
+
+
+print( query )
 
 --]=]
+
+
+return SQL
